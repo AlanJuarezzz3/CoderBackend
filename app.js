ff69f4fa5df7fe624a1c70abe5115a652a59b21d
AlanJuarezzz3 <alan.juarez_roman@hotmail.com>
2022-11-04 20:59:34 -0300

    actualizacion de plantilla

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.use("/static", express.static(__dirname + "/public"));
+app.set("view engine", "pug");
+app.set("views", "./views")
 
-const { Router } = express;
-
-const autosRouter = new Router();
-
-autosRouter.use(express.json());
-autosRouter.use(express.urlencoded({ extended: true }));
-
-const autos = [];
-
-autosRouter.get("/", (req, res) => {
-  res.json(autos);
-});
-
-function addId(req, res, next) {
-  req.body.id = autos.length + 1;
-  next();
-}
-
-autosRouter.post("/", addId, (req, res) => {
-  autos.push(req.body);
-  res.json(req.body);
-});
-
-autosRouter.delete("/:id", (req, res) => {
-  autos.splice(req.params.id - 1, 1);
-  res.json(mascotas);
-});
-
-app.use("/autos", autosRouter);
-
-app.use((req, res, next) => {
-  res.status(400).send("Error producto no encontrado");
-});
+app.use("/static", express.static(__dirname + "public"))
 
 const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http://localhost:${PORT}`)
+})
 
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+const { Router } = express;
+const productosRouter = new Router();
+
+productosRouter.use(express.json())
+productosRouter.use(express.urlencoded({ extended: true }))
+
+server.on("error", error => console.log(`Error en servidor ${error}`))
+
+const handleVerify=(atributo)=>{
+    return (atributo!="") 
+}
+
+const productos = []
+
+productosRouter.get("/", (req, res) => {
+    res.render("productos", {productos})
+})
+app.get("/", (req, res) => {
+    res.render("formulario",{titlePage:"Formulario"})
+})
+productosRouter.post("/", (req, res) => {
+    let objeto = req.body;
+    const veri = handleVerify(objeto.title)&&handleVerify(objeto.price)&&handleVerify(objeto.thumbnail);
+    if(veri){
+        if (productos.length != 0) {
+            let arrayId = productos.map(item => item.id);
+            let highId = Math.max(...arrayId);
+            objeto.id = highId + 1;
+        } else objeto.id = 1;
+        productos.push(objeto);
+    }
+    res.redirect('/')
+})
+
+app.use("/api/productos", productosRouter);
+
+app.use('/static', express.static('public'));
+
+app.use((req, res, next) => {
+    res.status(404).send("Pagina no encontrada");
+})
